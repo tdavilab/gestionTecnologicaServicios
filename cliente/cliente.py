c33464785787dd185102b1dab8586aa2482db9c0
Thomas Avila <thomasdanielavila@gmail.com>
2019-09-16 15:19:43 -0500

    Add files via upload

---
+++ cliente/cliente.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template,request,make_response
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/show_all')
+def show_all():
+    	return render_template('show_all.html')
+
+
+@app.route('/send', methods=['POST', 'GET'])
+def send():
+	signo = request.form['action']	
+	num1 = request.form["num1"]
+	num2 = request.form["num2"]
+	resultado=requests.get('http://app:5000/'+num1+'/'+num2+'/'+signo)
+	return render_template('/show_all.html', string_variable=resultado)
+
+if __name__ == "__main__":
+    	app.run(host="0.0.0.0", debug=True)
+
--- division/division.py
+++ division/division.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/<num1>/<num2>")
 
 def default(num1,num2):
-    return str(float(num1)+float(num2))
+    return str(float(num1)/float(num2))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
